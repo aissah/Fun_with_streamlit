8c6bbdc292e7f82c1c65d9af01ec9f532d528a56
Abdul Hafiz Saeed Issah <issahhafiz@gmail.com>
2024-05-15 15:34:13 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ import streamlit as st
 def main():
     st.title("Welcome")
 
-    st.write("Welcome to the Home Page of the Streamlit App")
+    st.write("Hello, my name is Hafiz. I am currently pursuing my PhD in Computational and Applied Mathematics at Colorado School of Mines. I created this web app as part of the process of learning to use Streamlit and Dask. I am hoping I can add more relevant projects that reflect my personal, academic, and research interests in the future.")
 
 
 if __name__ == "__main__":
